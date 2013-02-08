6bcadcfc81852da42683b71592a1ea8276ebb51f
Tb0 <iTbo@iMac-de-Thibault.local>
2013-02-08 23:17:42 +0100

    Add escape js method to close the lightbox

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,17 @@
-function HideContent(d) {
-		if(d.length < 1) { return; }
-		document.getElementById(d).style.display = "none";
-		}
-		function ShowContent(d) {
-		if(d.length < 1) { return; }
-		document.getElementById(d).style.display = "block";
-		}
-		function ReverseContentDisplay(d) {
-		if(d.length < 1) { return; }
-		if(document.getElementById(d).style.display == "none") { document.getElementById(d).style.display = "block"; }
-		else { document.getElementById(d).style.display = "none"; }
-		}
+function hideContent(d) {
+	if(d.length < 1) { return; }
+	document.getElementById(d).style.display = "none";
+}
 
+function showContent(d) {
+	if(d.length < 1) { return; }
+	document.getElementById(d).style.display = "block";
+	targeted_div = d;
+}
 
+function reverseContentDisplay(d) {
+	if(d.length < 1) { return; }
+	if(document.getElementById(d).style.display == "none") { document.getElementById(d).style.display = "block"; }
+	else { document.getElementById(d).style.display = "none"; }
+}
 
